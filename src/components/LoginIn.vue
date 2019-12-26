12ed561fc5d6d032ea554e3e6a367c9ab860a0cf
jspfei <jiangfeifan8@163.com>
2019-12-26 18:54:15 +0800

    login

---
+++ src/components/LoginIn.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <img src="@/assets/logo.png" />
+    </div>
+  </div>
+</template>
+  <script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+  <style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
+ 
